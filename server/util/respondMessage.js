c146e8b79b550cf2a0f67d1ceb5ad56f56c9f4bc
qQLQp <71122192+qQLQp@users.noreply.github.com>
2021-07-06 19:55:15 +0700

    only client

--- server/util/respondMessage.js
+++
@@ -1,9 +0,0 @@
-class resMess {
-  constructor(success, message, data = null) {
-    this.success = success;
-    this.message = message;
-    this.data = data;
-  }
-}
-
-module.exports = { resMess };
