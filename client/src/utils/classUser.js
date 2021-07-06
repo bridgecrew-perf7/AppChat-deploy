c7a8def963229eaa725a1554fd19251186f2c371
qQLQp <71122192+qQLQp@users.noreply.github.com>
2021-07-06 19:46:13 +0700

    only server

--- client/src/utils/classUser.js
+++
@@ -1,9 +0,0 @@
-class User {
-  constructor(phone, password, username = null) {
-    this.phone = phone;
-    this.password = password;
-    this.username = username ? username.trim() : username;
-  }
-}
-
-export default User;
